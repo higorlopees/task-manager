98a52e9649782642098149300999fb2de3ea8643
higorlopees <higorlopees@live.com>
2020-02-27 07:39:29 -0300

    Added task module and task model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TaskModule } from './task';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    TaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
