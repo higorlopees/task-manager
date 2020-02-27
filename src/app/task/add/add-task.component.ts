6a331b08806af5981e1996603f80d5b69b3fd5a6
higorlopees <higorlopees@live.com>
2020-02-27 09:18:11 -0300

    Added add-task interface

--- src/app/task/add/add-task.component.ts
+++ src/app/task/add/add-task.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { TaskService, Task } from '../shared';
 
 @Component({
   selector: 'app-add-task',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTaskComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('taskForm', { static: true }) taskForm: NgForm;
+  task: Task;
+
+  constructor(
+    private taskService: TaskService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.task = new Task();
   }
 
+  add(): void {
+    if(this.taskForm.form.valid){
+      this.taskService.add(this.task);
+      this.router.navigate(["/tasks"]);
+    }
+  }
 }
